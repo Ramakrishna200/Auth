5cb59383b9c4c21df3ecd08c427074e745b128a8
Ramakrishna mandadi <105452720+Ramakrishna200@users.noreply.github.com>
2024-06-23 20:59:27 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
-const bcrypt = require('bcrypt');
+// const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const session = require('express-session');
