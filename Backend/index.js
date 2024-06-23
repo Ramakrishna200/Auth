3c2e2df85b8bce456de667347be0f599dd3a1dd0
Ramakrishna mandadi <105452720+Ramakrishna200@users.noreply.github.com>
2024-06-23 20:46:53 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -9,7 +9,7 @@ const flash = require('connect-flash');
 const app = express();
 
 // MongoDB connection
-mongoose.connect('mongodb://localhost:27017/userDB')
+mongoose.connect('mongodb+srv://krishna:krishna123@cluster0.9dcqd2t.mongodb.net/?retryWrites=true&w=majority')
     .then(() => console.log('Connected to MongoDB'))
     .catch((err) => console.error('Failed to connect to MongoDB', err));
 
@@ -129,8 +129,14 @@ app.get('/dashboard', (req, res) => {
     res.render('dashboard', { success_msg: req.flash('success_msg') });
 });
 
+// Error handling middleware
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something broke!');
+});
+
 // Start the server
-const port = 5000;
+const port = process.env.PORT || 5000;
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
